03f7f63a45b502a5cbd3844b9c172fa5d65e8bb4
ujin16 <94890646+ujin16@users.noreply.github.com>
2023-07-28 14:14:12 +0900

    :truck: Edit: BrowserRouter -> HashRouter 변경경

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import GlobalStyles from "./styles/GlobalStyle";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { StartPage, SnapPage, FramePage, PrintPage, DownloadPage } from "./components/pages";
 import { useState } from "react";
 
@@ -9,15 +9,13 @@ function App() {
     return (
         <div className="App">
             <GlobalStyles />
-            <BrowserRouter>
-                <Routes>
-                    <Route path={process.env.PUBLIC_URL + "/"} element={<StartPage />} />
-                    <Route path={process.env.PUBLIC_URL + "/frame"} element={<FramePage />} />
-                    <Route path={process.env.PUBLIC_URL + "/snap"} element={<SnapPage setResult={setResult} />} />
-                    <Route path={process.env.PUBLIC_URL + "/print"} element={<PrintPage result={result} />} />
-                    <Route path={process.env.PUBLIC_URL + "/download/:imgUrl/*"} element={<DownloadPage />} />
-                </Routes>
-            </BrowserRouter>
+            <Routes>
+                <Route path={process.env.PUBLIC_URL + "/"} element={<StartPage />} />
+                <Route path={process.env.PUBLIC_URL + "/frame"} element={<FramePage />} />
+                <Route path={process.env.PUBLIC_URL + "/snap"} element={<SnapPage setResult={setResult} />} />
+                <Route path={process.env.PUBLIC_URL + "/print"} element={<PrintPage result={result} />} />
+                <Route path={process.env.PUBLIC_URL + "/download/:imgUrl/*"} element={<DownloadPage />} />
+            </Routes>
         </div>
     );
 }
