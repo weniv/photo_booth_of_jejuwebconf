eea6a582cc931a5fc8bb1f449554f2c433d8ddcb
ujin16 <94890646+ujin16@users.noreply.github.com>
2023-07-28 13:32:56 +0900

    :truck: Add: DownloadPage 컴포넌트 및 라우팅 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GlobalStyles from "./styles/GlobalStyle";
-import { StartPage, SnapPage, FramePage, PrintPage } from "./components/pages";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { StartPage, SnapPage, FramePage, PrintPage, DownloadPage } from "./components/pages";
 import { useState } from "react";
 
 function App() {
@@ -15,6 +15,7 @@ function App() {
                     <Route path={process.env.PUBLIC_URL + "/frame"} element={<FramePage />} />
                     <Route path={process.env.PUBLIC_URL + "/snap"} element={<SnapPage setResult={setResult} />} />
                     <Route path={process.env.PUBLIC_URL + "/print"} element={<PrintPage result={result} />} />
+                    <Route path={process.env.PUBLIC_URL + "/download/:imgUrl/*"} element={<DownloadPage />} />
                 </Routes>
             </BrowserRouter>
         </div>
---
+++ src/components/pages/DownloadPage.jsx
@@ -0,0 +1,6 @@
+import { useParams } from "react-router-dom";
+
+export default function DownloadPage() {
+    let { imgUrl } = useParams();
+    return <></>;
+}
